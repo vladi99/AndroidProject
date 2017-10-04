ec4d9bc16890af5256f498244714743dfb834b11
vladi99 <vlpetrovic99@gmail.com>
2017-10-04 15:49:02 +0300

    video playing

--- YoutubeConverter/app/src/main/java/vladi/youtubeconverter/PlayVideo.java
+++ YoutubeConverter/app/src/main/java/vladi/youtubeconverter/PlayVideo.java
@@ -1,8 +1,12 @@
 package vladi.youtubeconverter;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import java.io.IOException;
 
 public class PlayVideo extends AppCompatActivity {
 
@@ -14,7 +18,19 @@ public class PlayVideo extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         String path = getIntent().getStringExtra("VIDEO_PATH");
-        System.out.println(path);
+        MediaPlayer mp = new MediaPlayer();
+        mp.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        try {
+            mp.setDataSource(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            mp.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mp.start();
     }
 
 }
